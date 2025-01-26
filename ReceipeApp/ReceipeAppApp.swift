5d110ecce04eb72c8b47fb54b7405ad349780147
matghazaryan <matevos14@gmail.com>
2025-01-25 22:50:41 -0800

    Initial Commit

---
+++ ReceipeApp/ReceipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppApp.swift
+//  ReceipeApp
+//
+//  Created by Matevos Ghazaryan on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ReceipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceipeAppTests/ReceipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppTests.swift
+//  ReceipeAppTests
+//
+//  Created by Matevos Ghazaryan on 1/25/25.
+//
+
+import Testing
+@testable import ReceipeApp
+
+struct ReceipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
